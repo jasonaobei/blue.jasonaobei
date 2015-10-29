8bb8e33bc6351fa56d58e906b09d76fe97dd0477
hwgc <yanjz@hanwang.com.cn>
2015-10-29 11:59:49 +0800

    ibule project
    create subclass iblue repositorfier

---
+++ ibule/ibule/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ibule
+//
+//  Created by hwgc on 15/10/29.
+//  Copyright (c) 2015年 hwgc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
